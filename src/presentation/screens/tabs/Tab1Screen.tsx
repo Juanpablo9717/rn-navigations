d4454c24b06e7121fb7849787f554ffaf12fb265
Juanpablo9717 <juanpablo9717@gmail.com>
2024-06-30 13:17:52 -0500

    ANDROID vector icons setup

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import HamburguerMenu from '../../components/shared/HamburguerMenu';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab1Screen = () => {
   return (
     <View>
       <HamburguerMenu />
       <Text>Tab1Screen</Text>
+      <Icon name="rocket-outline" size={100} />
     </View>
   );
 };
