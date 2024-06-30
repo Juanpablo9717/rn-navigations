6fc6b48c39aeef244eb04b4ecbe957bcb6903a0a
Juanpablo9717 <juanpablo9717@gmail.com>
2024-06-29 23:02:22 -0500

    useSafeAreaInserts [Drawer navigation final setup]

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,10 +1,27 @@
-import {View, Text} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
+import {View, Text} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import PrimaryButton from '../../components/shared/PrimaryButton';
+import {DrawerActions, useNavigation} from '@react-navigation/native';
 
 const ProfileScreen = () => {
+  const {top} = useSafeAreaInsets();
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: 20,
+        marginTop: top,
+      }}>
+      <Text style={{marginBottom: 20}}>ProfileScreen</Text>
+
+      <PrimaryButton
+        label="Abrir Menu"
+        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+      />
     </View>
   );
 };
