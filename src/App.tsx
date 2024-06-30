3a562308c24e91721b4ac8acd6d30d2ecf24da47
Juanpablo9717 <juanpablo9717@gmail.com>
2024-06-29 19:50:46 -0500

    react navigation installed - pre-requisites

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import {Text, View} from 'react-native';
+import {Text} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={{flex: 1}}>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Text>App.TSX --------</Text>
+    </NavigationContainer>
   );
 }
